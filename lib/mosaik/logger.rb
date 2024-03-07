80b585234d79eb6b4d431316ac383909f53fe69a
Florian Dejonckheere <florian@floriandejonckheere.be>
2024-03-07 21:17:10 +0100

    Allow setting log level

--- lib/mosaik/logger.rb
+++ lib/mosaik/logger.rb
@@ -8,18 +8,22 @@ require "colorize"
 module MOSAIK
   class Logger < SimpleDelegator
     def initialize
-      super(::Logger.new($stdout, formatter:))
+      super(::Logger.new($stdout, level:, formatter:))
+    end
+
+    def level
+      ENV.fetch("LOG_LEVEL", "info")
     end
 
     private
 
     def formatter
       proc do |severity, _time, _progname, msg|
-        abort("#{File.basename($PROGRAM_NAME)}: #{msg}".white.on_red) if severity == "FATAL"
+        abort("#{File.basename($PROGRAM_NAME)}: #{msg}".white.on_red) if severity == ::Logger::FATAL
 
         msg = "#{msg}\n"
-        msg = msg.yellow if severity == "DEBUG"
-        msg = msg.red if severity == "ERROR"
+        msg = msg.yellow if severity == ::Logger::DEBUG
+        msg = msg.red if severity == ::Logger::ERROR
 
         msg
       end

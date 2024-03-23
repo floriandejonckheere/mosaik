1658edf147aeffaee9c2378cee83a779e1c81901
Florian Dejonckheere <florian@floriandejonckheere.be>
2024-03-23 11:26:02 +0200

    Enumerate files according to configuration

--- lib/mosaik/commands/collect.rb
+++ lib/mosaik/commands/collect.rb
@@ -6,7 +6,13 @@ module MOSAIK
       self.description = "Collect data"
 
       def start
-        info "Starting data collection..."
+        include_patterns = MOSAIK.configuration.include.map { |i| "#{MOSAIK.options.directory}/#{i}" }
+        exclude_patterns = MOSAIK.configuration.exclude.map { |e| "#{MOSAIK.options.directory}/#{e}" }
+
+        included_files = Dir[*include_patterns]
+        excluded_files = Dir[*exclude_patterns]
+
+        included_files - excluded_files
       end
     end
   end
--- spec/mosaik/commands/collect_spec.rb
+++ spec/mosaik/commands/collect_spec.rb
@@ -2,8 +2,4 @@
 
 RSpec.describe MOSAIK::Commands::Collect do
   subject(:command) { build(:collect_command) }
-
-  it "shows the current application version" do
-    expect { command.start }.to log(/Starting data collection.../o)
-  end
 end

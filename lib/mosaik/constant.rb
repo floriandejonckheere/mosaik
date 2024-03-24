e4c25db7a390fea50aaeb26a7a53c6d64ea1a324
Florian Dejonckheere <florian@floriandejonckheere.be>
2024-03-24 14:19:41 +0200

    Constant#methods should not be a hash creating an array

--- lib/mosaik/constant.rb
+++ lib/mosaik/constant.rb
@@ -7,7 +7,11 @@ module MOSAIK
     def initialize(name)
       @name = name
 
-      @methods = Hash.new { |h, k| h[k] = [] }
+      @methods = Hash.new
+    end
+
+    def add_method(method_name, file = nil, line_num = nil)
+      methods[method_name] = Method.new(self, method_name, file, line_num)
     end
 
     def ==(other)
--- lib/mosaik/processors/ruby.rb
+++ lib/mosaik/processors/ruby.rb
@@ -58,7 +58,7 @@ module MOSAIK
 
         debug "Class instance method #{current_class}##{method_name} in #{file}:#{line_num}"
 
-        registry[current_class].methods[method_name] = Method.new(registry[current_class], method_name, file, line_num)
+        registry[current_class].add_method(method_name, file, line_num)
 
         # Traverse the AST (first two children are method name and arguments)
         node.children[2..].each { |c| process(c) }
@@ -74,7 +74,7 @@ module MOSAIK
 
         debug "Class method #{current_class}.#{node.children[1]} in #{file}:#{line_num}"
 
-        registry[current_class].methods[method_name] = Method.new(registry[current_class], method_name, file, line_num)
+        registry[current_class].add_method(method_name, file, line_num)
 
         # Traverse the AST (first two children are method name and arguments)
         node.children[2..].each { |c| process(c) }

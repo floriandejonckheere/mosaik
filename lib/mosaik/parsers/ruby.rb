d3f6d0c1f7a0ac83a33fefb630abb8d83522d6f9
Florian Dejonckheere <florian@floriandejonckheere.be>
2024-03-24 11:19:27 +0200

    Parse method calls in Ruby files

--- lib/mosaik/parsers/ruby.rb
+++ lib/mosaik/parsers/ruby.rb
@@ -6,9 +6,11 @@ module MOSAIK
       def parse(file, registry)
         debug "Parsing file: #{file}"
 
+        # Parse Abstract Syntax Tree
         ast = Parser::Ruby33
           .parse_file(file)
 
+        # Process AST to extract constants, methods and references
         Processors::Ruby
           .new(registry)
           .process(ast)
--- lib/mosaik/processors/ruby.rb
+++ lib/mosaik/processors/ruby.rb
@@ -4,7 +4,7 @@ module MOSAIK
   module Processors
     class Ruby < AST::Processor
       attr_reader :registry
-      attr_accessor :current_class
+      attr_accessor :current_class, :current_method
 
       def initialize(registry)
         super()
@@ -40,24 +40,75 @@ module MOSAIK
 
       # Instance methods
       def on_def(node)
-        line_num = node.loc.line
-        file = node.loc.expression.source_buffer.name
         method_name = node.children[0].to_s
+        file = node.loc.expression.source_buffer.name
+        line_num = node.loc.line
+
+        self.current_method = method_name
+
+        # Traverse the AST (first two children are method name and arguments)
+        node.children[2..].each { |c| process(c) }
 
         registry.constants[current_class].methods[method_name] = Method.new(method_name, file, line_num)
       end
 
       # Class methods
       def on_defs(node)
-        line_num = node.loc.line
-        file = node.loc.expression.source_buffer.name
         method_name = "self.#{node.children[1]}"
+        file = node.loc.expression.source_buffer.name
+        line_num = node.loc.line
+
+        self.current_method = method_name
+
+        # Traverse the AST (first two children are method name and arguments)
+        node.children[2..].each { |c| process(c) }
 
         registry.constants[current_class].methods[method_name] = Method.new(method_name, file, line_num)
       end
 
+      # Method bodies
       def on_begin(node)
         node.children.each { |c| process(c) }
+      end
+
+      # Method calls
+      def on_send(node)
+        receiver, callee = method_from(node)
+
+        constant_name = constant_name_from(receiver)
+
+        debug "Reference to #{constant_name}##{callee} from #{current_class}##{current_method} in #{node.loc.expression.source_buffer.name}:#{node.loc.line}"
+      end
+
+      private
+
+      def method_from(node)
+        receiver = node.children[0]
+        callee = node.children[1]
+
+        # If the receiver is a send, descend to the child node
+        # We are only interested in the first callee
+        return method_from(receiver) if receiver.type == :send
+
+        [receiver, callee]
+      end
+
+      def constant_name_from(node)
+        # If current node is a send, descend to the child node
+        return constant_name_from(node.children[0]) if node&.type == :send
+
+        # If current node is not a constant, return empty string
+        return "" unless node&.type == :const
+
+        # Current constant name
+        name = node.children[1].to_s
+
+        # Process the child node to build the namespace prefix
+        prefix = constant_name_from(node.children[0])
+
+        # Combine the prefix and the current name with a scope resolution operator (::)
+        # If there's no prefix (i.e., we're at the top level), don't prepend ::
+        prefix.empty? ? name : "#{prefix}::#{name}"
       end
     end
   end

884a0455923e1d071a9cf2e796180ac990e346c7
Florian Dejonckheere <florian@floriandejonckheere.be>
2024-03-30 20:32:11 +0200

    Add (un-)directed graph

--- lib/mosaik/graph/graph.rb
+++ lib/mosaik/graph/graph.rb
@@ -17,12 +17,24 @@ module MOSAIK
         vertices[vertex.value] = vertex
       end
 
-      def add_edge(from, to, weight = nil)
+      def add_directed_edge(from, to, weight = nil)
         vertices[from.value].add_edge(to, weight)
       end
+      alias add_edge add_directed_edge
 
-      def remove_edge(from, to)
+      def add_undirected_edge(from, to, weight = nil)
+        vertices[from.value].add_edge(to, weight)
+        vertices[to.value].add_edge(from, weight)
+      end
+
+      def remove_directed_edge(from, to)
         vertices[from.value].remove_edge(to)
+      end
+      alias remove_edge remove_directed_edge
+
+      def remove_undirected_edge(from, to)
+        vertices[from.value].remove_edge(to)
+        vertices[to.value].remove_edge(from)
       end
 
       def find_vertex(value)
--- spec/mosaik/graph/graph_spec.rb
+++ spec/mosaik/graph/graph_spec.rb
@@ -13,51 +13,51 @@ RSpec.describe MOSAIK::Graph::Graph do
     end
   end
 
-  describe "#add_edge" do
-    it "adds an edge" do
+  describe "#add_edge, #add_directed_edge" do
+    it "adds a directed edge" do
       vertex1 = build(:vertex, value: "vertex1")
       vertex2 = build(:vertex, value: "vertex2")
 
       graph.add_vertex(vertex1)
       graph.add_vertex(vertex2)
-      graph.add_edge(vertex1, vertex2)
+      graph.add_directed_edge(vertex1, vertex2)
 
       expect(graph.find_vertex("vertex1").edges.keys).to eq ["vertex2"]
     end
 
-    it "does not add an edge twice" do
+    it "does not add a directed edge twice" do
       vertex1 = build(:vertex, value: "vertex1")
       vertex2 = build(:vertex, value: "vertex2")
 
       graph.add_vertex(vertex1)
       graph.add_vertex(vertex2)
-      graph.add_edge(vertex1, vertex2)
-      graph.add_edge(vertex1, vertex2)
+      graph.add_directed_edge(vertex1, vertex2)
+      graph.add_directed_edge(vertex1, vertex2)
 
       expect(graph.find_vertex("vertex1").edges.keys).to eq ["vertex2"]
     end
 
     describe "weighted" do
-      it "adds a weighted edge" do
+      it "adds a directed weighted edge" do
         vertex1 = build(:vertex, value: "vertex1")
         vertex2 = build(:vertex, value: "vertex2")
 
         graph.add_vertex(vertex1)
         graph.add_vertex(vertex2)
-        graph.add_edge(vertex1, vertex2, 5)
+        graph.add_directed_edge(vertex1, vertex2, 5)
 
         expect(graph.find_vertex("vertex1").edges.keys).to eq ["vertex2"]
         expect(graph.find_vertex("vertex1").edges["vertex2"]).to eq 5
       end
 
-      it "overrides a weighted edge" do
+      it "overrides a directed weighted edge" do
         vertex1 = build(:vertex, value: "vertex1")
         vertex2 = build(:vertex, value: "vertex2")
 
         graph.add_vertex(vertex1)
         graph.add_vertex(vertex2)
-        graph.add_edge(vertex1, vertex2, 5)
-        graph.add_edge(vertex1, vertex2, 10)
+        graph.add_directed_edge(vertex1, vertex2, 5)
+        graph.add_directed_edge(vertex1, vertex2, 10)
 
         expect(graph.find_vertex("vertex1").edges.keys).to eq ["vertex2"]
         expect(graph.find_vertex("vertex1").edges["vertex2"]).to eq 10
@@ -65,54 +65,160 @@ RSpec.describe MOSAIK::Graph::Graph do
     end
   end
 
-  describe "#remove_edge" do
-    it "removes an edge" do
+  describe "#add_undirected_edge" do
+    it "adds an undirected edge" do
       vertex1 = build(:vertex, value: "vertex1")
       vertex2 = build(:vertex, value: "vertex2")
 
       graph.add_vertex(vertex1)
       graph.add_vertex(vertex2)
-      graph.add_edge(vertex1, vertex2)
+      graph.add_undirected_edge(vertex1, vertex2)
 
-      graph.remove_edge(vertex1, vertex2)
+      expect(graph.find_vertex("vertex1").edges.keys).to eq ["vertex2"]
+    end
+
+    it "does not add an undirected edge twice" do
+      vertex1 = build(:vertex, value: "vertex1")
+      vertex2 = build(:vertex, value: "vertex2")
+
+      graph.add_vertex(vertex1)
+      graph.add_vertex(vertex2)
+      graph.add_undirected_edge(vertex1, vertex2)
+      graph.add_undirected_edge(vertex1, vertex2)
+
+      expect(graph.find_vertex("vertex1").edges.keys).to eq ["vertex2"]
+    end
+
+    describe "weighted" do
+      it "adds an undirected weighted edge" do
+        vertex1 = build(:vertex, value: "vertex1")
+        vertex2 = build(:vertex, value: "vertex2")
+
+        graph.add_vertex(vertex1)
+        graph.add_vertex(vertex2)
+        graph.add_undirected_edge(vertex1, vertex2, 5)
+
+        expect(graph.find_vertex("vertex1").edges.keys).to eq ["vertex2"]
+        expect(graph.find_vertex("vertex1").edges["vertex2"]).to eq 5
+      end
+
+      it "overrides an undirected weighted edge" do
+        vertex1 = build(:vertex, value: "vertex1")
+        vertex2 = build(:vertex, value: "vertex2")
+
+        graph.add_vertex(vertex1)
+        graph.add_vertex(vertex2)
+        graph.add_undirected_edge(vertex1, vertex2, 5)
+        graph.add_undirected_edge(vertex1, vertex2, 10)
+
+        expect(graph.find_vertex("vertex1").edges.keys).to eq ["vertex2"]
+        expect(graph.find_vertex("vertex1").edges["vertex2"]).to eq 10
+      end
+    end
+  end
+
+  describe "#remove_edge, #remove_directed_edge" do
+    it "removes a directed edge" do
+      vertex1 = build(:vertex, value: "vertex1")
+      vertex2 = build(:vertex, value: "vertex2")
+
+      graph.add_vertex(vertex1)
+      graph.add_vertex(vertex2)
+      graph.add_directed_edge(vertex1, vertex2)
+
+      graph.remove_directed_edge(vertex1, vertex2)
 
       expect(graph.find_vertex("vertex1").edges).to be_empty
     end
 
-    it "does not remove a non-existing edge" do
+    it "does not remove a non-existing directed edge" do
       vertex1 = build(:vertex, value: "vertex1")
       vertex2 = build(:vertex, value: "vertex2")
 
       graph.add_vertex(vertex1)
       graph.add_vertex(vertex2)
 
-      graph.remove_edge(vertex1, vertex2)
+      graph.remove_directed_edge(vertex1, vertex2)
 
       expect(graph.find_vertex("vertex1").edges).to be_empty
     end
 
     describe "weighted" do
-      it "removes a weighted edge" do
+      it "removes a directed weighted edge" do
         vertex1 = build(:vertex, value: "vertex1")
         vertex2 = build(:vertex, value: "vertex2")
 
         graph.add_vertex(vertex1)
         graph.add_vertex(vertex2)
-        graph.add_edge(vertex1, vertex2, 5)
+        graph.add_directed_edge(vertex1, vertex2, 5)
 
-        graph.remove_edge(vertex1, vertex2)
+        graph.remove_directed_edge(vertex1, vertex2)
 
         expect(graph.find_vertex("vertex1").edges).to be_empty
       end
 
-      it "does not remove a non-existing weighted edge" do
+      it "does not remove a non-existing directed weighted edge" do
         vertex1 = build(:vertex, value: "vertex1")
         vertex2 = build(:vertex, value: "vertex2")
 
         graph.add_vertex(vertex1)
         graph.add_vertex(vertex2)
 
-        graph.remove_edge(vertex1, vertex2)
+        graph.remove_directed_edge(vertex1, vertex2)
+
+        expect(graph.find_vertex("vertex1").edges).to be_empty
+      end
+    end
+  end
+
+  describe "#remove_undirected_edge" do
+    it "removes an undirected edge" do
+      vertex1 = build(:vertex, value: "vertex1")
+      vertex2 = build(:vertex, value: "vertex2")
+
+      graph.add_vertex(vertex1)
+      graph.add_vertex(vertex2)
+      graph.add_undirected_edge(vertex1, vertex2)
+
+      graph.remove_undirected_edge(vertex1, vertex2)
+
+      expect(graph.find_vertex("vertex1").edges).to be_empty
+    end
+
+    it "does not remove a non-existing directed edge" do
+      vertex1 = build(:vertex, value: "vertex1")
+      vertex2 = build(:vertex, value: "vertex2")
+
+      graph.add_vertex(vertex1)
+      graph.add_vertex(vertex2)
+
+      graph.remove_undirected_edge(vertex1, vertex2)
+
+      expect(graph.find_vertex("vertex1").edges).to be_empty
+    end
+
+    describe "weighted" do
+      it "removes a directed weighted edge" do
+        vertex1 = build(:vertex, value: "vertex1")
+        vertex2 = build(:vertex, value: "vertex2")
+
+        graph.add_vertex(vertex1)
+        graph.add_vertex(vertex2)
+        graph.add_directed_edge(vertex1, vertex2, 5)
+
+        graph.remove_directed_edge(vertex1, vertex2)
+
+        expect(graph.find_vertex("vertex1").edges).to be_empty
+      end
+
+      it "does not remove a non-existing directed weighted edge" do
+        vertex1 = build(:vertex, value: "vertex1")
+        vertex2 = build(:vertex, value: "vertex2")
+
+        graph.add_vertex(vertex1)
+        graph.add_vertex(vertex2)
+
+        graph.remove_directed_edge(vertex1, vertex2)
 
         expect(graph.find_vertex("vertex1").edges).to be_empty
       end

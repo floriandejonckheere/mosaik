3a14f13d51bfae7319e34b9e603c290fd28aeb37
Florian Dejonckheere <florian@floriandejonckheere.be>
2024-04-13 13:37:49 +0300

    Move simple graph to shared context

--- spec/mosaik/metrics/modularity_spec.rb
+++ spec/mosaik/metrics/modularity_spec.rb
@@ -3,42 +3,7 @@
 RSpec.describe MOSAIK::Metrics::Modularity do
   subject(:metric) { build(:modularity_metric, graph:) }
 
-  let(:graph) do
-    graph = build(:graph, directed: false)
-
-    a = graph.add_vertex("A")
-    b = graph.add_vertex("B")
-    c = graph.add_vertex("C")
-    d = graph.add_vertex("D")
-    e = graph.add_vertex("E")
-    f = graph.add_vertex("F")
-
-    graph.add_edge("A", "B", weight: 3.0)
-    graph.add_edge("A", "C", weight: 1.0)
-    graph.add_edge("A", "D", weight: 5.0)
-
-    graph.add_edge("B", "C", weight: 1.0)
-    graph.add_edge("B", "D", weight: 4.0)
-
-    graph.add_edge("C", "D", weight: 1.0)
-    graph.add_edge("C", "E", weight: 5.0)
-
-    graph.add_edge("D", "F", weight: 2.0)
-
-    c1 = graph.add_cluster("A")
-    c1.add_vertex(a)
-    c1.add_vertex(b)
-    c1.add_vertex(d)
-
-    c2 = graph.add_cluster("B")
-    c2.add_vertex(c)
-    c2.add_vertex(e)
-
-    c3 = graph.add_cluster("C")
-    c3.add_vertex(f)
-
-    graph
-  end
+  include_context "with a simple undirected graph"
 
   # Modularity value calculated by hand:
   #

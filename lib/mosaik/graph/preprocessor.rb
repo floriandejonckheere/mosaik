b6c25b97d4e0af8b82b0a4d3de3ac9088f65596c
Florian Dejonckheere <florian@floriandejonckheere.be>
2024-04-17 16:53:54 +0300

    Add graph preprocessor step

--- lib/mosaik/graph/preprocessor.rb
+++ lib/mosaik/graph/preprocessor.rb
@@ -15,7 +15,43 @@ module MOSAIK
         @graph = graph
       end
 
-      def call; end
+      def call
+        # Iterate over all combinations of vertices
+        weights = graph.vertices.keys.combination(2).map do |v1, v2|
+          # Find all edges between the two vertices
+          edges = graph.find_edges(v1, v2) + graph.find_edges(v2, v1)
+
+          # Calculate the weight for the aggregate edge
+          weight = 0.0
+
+          # Add weight for structural coupling
+          weight += options[:structural] * edges
+            .select { |e| e.attributes[:type] == "structural" }
+            .sum { |e| e.attributes.fetch(:weight, 0.0) }
+
+          # Add weight for logical coupling
+          weight += options[:logical] * edges
+            .select { |e| e.attributes[:type] == "logical" }
+            .sum { |e| e.attributes.fetch(:weight, 0.0) }
+
+          # Add weight for contributor coupling
+          weight += options[:contributor] * edges
+            .select { |e| e.attributes[:type] == "contributor" }
+            .sum { |e| e.attributes.fetch(:weight, 0.0) }
+
+          # Return vertices and weights
+          [v1, v2, weight]
+        end
+
+        # Remove all existing edges
+        graph.vertices.each_value { |v| v.edges.clear }
+
+        # Set graph to undirected
+        graph.directed = false
+
+        # Add new edges
+        weights.each { |v1, v2, weight| graph.add_edge(v1, v2, weight:) }
+      end
     end
   end
 end
---
+++ spec/mosaik/graph/preprocessor_spec.rb
@@ -0,0 +1,94 @@
+# frozen_string_literal: true
+# typed: true
+
+RSpec.describe MOSAIK::Graph::Preprocessor do
+  subject(:preprocessor) { described_class.new(options, graph) }
+
+  let(:options) { { structural: 0.5, logical: 0.3, contributor: 0.2 } }
+  let(:graph) { build(:graph) }
+
+  before do
+    graph.add_vertex("v1")
+    graph.add_vertex("v2")
+    graph.add_vertex("v3")
+
+    graph.add_edge("v1", "v2", type: "structural", weight: 2, method: "read")
+    graph.add_edge("v1", "v2", type: "structural", weight: 3, method: "write")
+    graph.add_edge("v2", "v3", type: "structural", weight: 3, method: "read")
+    graph.add_edge("v3", "v2", type: "structural", weight: 1, method: "write")
+
+    graph.add_edge("v1", "v2", type: "logical", weight: 2)
+    graph.add_edge("v1", "v2", type: "logical", weight: 3)
+    graph.add_edge("v2", "v3", type: "logical", weight: 3)
+    graph.add_edge("v3", "v2", type: "logical", weight: 1)
+
+    graph.add_edge("v1", "v2", type: "contributor", weight: 2)
+    graph.add_edge("v1", "v2", type: "contributor", weight: 3)
+    graph.add_edge("v2", "v3", type: "contributor", weight: 3)
+    graph.add_edge("v3", "v2", type: "contributor", weight: 1)
+  end
+
+  it "aggregates the weights of the edges between vertices" do
+    preprocessor.call
+
+    v1_v2 = ((0.5 * 2) + (0.5 * 3)) + ((0.3 * 2) + (0.3 * 3)) + ((0.2 * 2) + (0.2 * 3))
+    v2_v3 = ((0.5 * 3) + (0.5 * 1)) + ((0.3 * 3) + (0.3 * 1)) + ((0.2 * 3) + (0.2 * 1))
+
+    expect(graph.find_edge("v1", "v2").attributes[:weight]).to eq v1_v2
+    expect(graph.find_edge("v2", "v1").attributes[:weight]).to eq v1_v2
+
+    expect(graph.find_edge("v2", "v3").attributes[:weight]).to eq v2_v3
+    expect(graph.find_edge("v3", "v2").attributes[:weight]).to eq v2_v3
+  end
+
+  describe "structural coupling" do
+    let(:options) { { structural: 0.5, logical: 0.0, contributor: 0.0 } }
+
+    it "aggregates the weights of the edges between vertices" do
+      preprocessor.call
+
+      v1_v2 = (0.5 * 2) + (0.5 * 3)
+      v2_v3 = (0.5 * 3) + (0.5 * 1)
+
+      expect(graph.find_edge("v1", "v2").attributes[:weight]).to eq v1_v2
+      expect(graph.find_edge("v2", "v1").attributes[:weight]).to eq v1_v2
+
+      expect(graph.find_edge("v2", "v3").attributes[:weight]).to eq v2_v3
+      expect(graph.find_edge("v3", "v2").attributes[:weight]).to eq v2_v3
+    end
+  end
+
+  describe "logical coupling" do
+    let(:options) { { structural: 0.0, logical: 0.3, contributor: 0.0 } }
+
+    it "aggregates the weights of the edges between vertices" do
+      preprocessor.call
+
+      v1_v2 = (0.3 * 2) + (0.3 * 3)
+      v2_v3 = (0.3 * 3) + (0.3 * 1)
+
+      expect(graph.find_edge("v1", "v2").attributes[:weight]).to eq v1_v2
+      expect(graph.find_edge("v2", "v1").attributes[:weight]).to eq v1_v2
+
+      expect(graph.find_edge("v2", "v3").attributes[:weight]).to eq v2_v3
+      expect(graph.find_edge("v3", "v2").attributes[:weight]).to eq v2_v3
+    end
+  end
+
+  describe "contributor coupling" do
+    let(:options) { { structural: 0.0, logical: 0.0, contributor: 0.1 } }
+
+    it "aggregates the weights of the edges between vertices" do
+      preprocessor.call
+
+      v1_v2 = (0.1 * 2) + (0.1 * 3)
+      v2_v3 = (0.1 * 3) + (0.1 * 1)
+
+      expect(graph.find_edge("v1", "v2").attributes[:weight]).to eq v1_v2
+      expect(graph.find_edge("v2", "v1").attributes[:weight]).to eq v1_v2
+
+      expect(graph.find_edge("v2", "v3").attributes[:weight]).to eq v2_v3
+      expect(graph.find_edge("v3", "v2").attributes[:weight]).to eq v2_v3
+    end
+  end
+end

3da9fd0e32797c80fb457109ad984462d650ab13
Florian Dejonckheere <florian@floriandejonckheere.be>
2024-04-17 17:23:04 +0300

    Add comma

--- lib/mosaik/graph/graph.rb
+++ lib/mosaik/graph/graph.rb
@@ -167,7 +167,7 @@ module MOSAIK
                     edge.attributes.any? ? " [label=\"#{edge.attributes.map { |ek, ev| "#{ek}: #{ev}" }.join(', ')}\"]" : nil,
                   ].compact.join
                 end
-              end
+              end,
             ].compact_blank.join("\n  ")
           end.compact_blank.join("\n  ").prepend("  "),
           "}\n",

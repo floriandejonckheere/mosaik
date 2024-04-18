99a7baf8f7f6467c6e6098ea449ce501cfa2a0e6
Florian Dejonckheere <florian@floriandejonckheere.be>
2024-04-18 19:22:53 +0300

    Move graph output arguments to superclass

--- lib/mosaik/command.rb
+++ lib/mosaik/command.rb
@@ -7,6 +7,12 @@ module MOSAIK
   class Command
     include Arguments
 
+    defaults hide_uncoupled: false,
+             hide_labels: false
+
+    argument("--hide-uncoupled", "Hide uncoupled vertices in the graph (default: #{defaults[:hide_uncoupled]})") { |arg| !arg.nil? }
+    argument("--hide-labels", "Hide labels in the graph (default: #{defaults[:hide_labels]})") { |arg| !arg.nil? }
+
     def validate; end
 
     def call; end

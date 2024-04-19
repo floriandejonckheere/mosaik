bf00ada6b0f2870e25c3399c8a8e64950645c52c
Florian Dejonckheere <florian@floriandejonckheere.be>
2024-04-19 09:54:58 +0300

    Revert "Use Rubocop's AST extension"
    This reverts commit 3505e5c12c55c4aa6d7357885a110e3c15832f5e.

--- lib/mosaik/extractors/structural/parser.rb
+++ lib/mosaik/extractors/structural/parser.rb
@@ -11,16 +11,13 @@ module MOSAIK
           debug "Parsing file: #{file}"
 
           # Parse Abstract Syntax Tree
-          source = RuboCop::AST::ProcessedSource
-            .new(File.read(file), 3.3)
+          ast = ::Parser::Ruby33
+            .parse_file(file)
 
           # Process AST to extract constants, methods and references
-          processor = Processor.new(tree)
-
-          source.ast.each_node do |node|
-            processor
-              .process(node)
-          end
+          Processor
+            .new(tree)
+            .process(ast)
         end
       end
     end

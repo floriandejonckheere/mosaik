6cfde3bf805ef1bc3d5e6f0f7cbff0e15bfa6db2
Florian Dejonckheere <florian@floriandejonckheere.be>
2024-04-20 12:31:07 +0300

    Add ActiveSupport inflections

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
 
-MOSAIK.loader.inflector.inflect({
-                                  "abc_size" => "ABCSize",
-                                  "cli" => "CLI",
-                                  "mosaik" => "MOSAIK",
-                                })
+MOSAIK.loader.inflector.inflect(
+  "abc_size" => "ABCSize",
+  "cli" => "CLI",
+  "mosaik" => "MOSAIK",
+)
+
+ActiveSupport::Inflector.inflections do |inflect|
+  inflect.acronym "ABC"
+  inflect.acronym "CLI"
+  inflect.acronym "MOSAIK"
+end
